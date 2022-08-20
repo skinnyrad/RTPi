2ec9bc90e6346a6544ac934273ef63a2f283c396
Skinny R&D <jason@skinnyrd.com>
2022-08-20 18:05:47 -0500

    Delete start.sh

--- start.sh
+++
@@ -1,8 +0,0 @@
-t1=$(ip a | grep -o eth1 | head -1)
-t2='eth1'
-if [ "$t1" = "$t2" ]; then
-  python ~/RTPi/rtpled.py eth1 &
-else
-  python ~/RTPi/rtpled.py eth0 &
-fi
-aplay -t raw -c 1 -f MU_LAW -r 8000 ~/RTPi/rtppipe &
