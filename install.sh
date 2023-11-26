71dd93181f37e421992277e0e671f6418b713d7e
Jason Baird <130998848+SkinnyRD@users.noreply.github.com>
2023-11-25 20:36:45 -0600

    Update install.sh
    made script work for any version of python3

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-
+version=$(ls /usr/bin/python*.* | head -1)
 cd ~
 sudo apt-get update
 curl https://get.pimoroni.com/blinkt | bash
 sudo apt-get install tcpdump
-sudo pip install scapy
+sudo apt-get install scapy
 mkfifo ~/RTPi/rtppipe
 
 #Set python and tcpdump so they don't need to use admin to run
-sudo setcap cap_net_raw=eip /usr/bin/python3.9
+sudo setcap cap_net_raw=eip $version
 sudo setcap cap_net_raw=eip /usr/bin/tcpdump
