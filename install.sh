9f62e47cdc7a8a607ef09fc834b91d0e5d9183d6
Jason Baird <130998848+SkinnyRD@users.noreply.github.com>
2026-04-14 15:41:59 -0500

    Update install.sh
    Major changes to everything

--- install.sh
+++ install.sh
@@ -2,11 +2,18 @@
 version=$(ls /usr/bin/python*.* | head -1)
 cd ~
 sudo apt-get update
-curl https://get.pimoroni.com/blinkt | bash
-sudo apt-get install tcpdump
-sudo apt-get install scapy
+sudo apt-get -y install tcpdump sox libsox-fmt-all scapy
 mkfifo ~/RTPi/rtppipe
 
 #Set python and tcpdump so they don't need to use admin to run
 sudo setcap cap_net_raw=eip $version
 sudo setcap cap_net_raw=eip /usr/bin/tcpdump
+
+#Installing Python Virtual Environment stuff
+cd RTPi
+git clone https://github.com/pimoroni/blinkt -b repackage
+python -m venv venv
+source venv/bin/activate
+cd blinkt
+./install.sh --unstable
+pip install scapy
